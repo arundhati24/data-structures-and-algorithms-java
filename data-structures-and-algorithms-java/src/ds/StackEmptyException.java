bb7f37afc1af22d666281ef8ce526d7fb2c47796
arundhati24 <arundhatigupta2411@gmail.com>
2017-12-13 19:04:37 +0530

    Add class StackEmptyException
    Extends java class RuntimeException. Handles the StackEmptyException
    i.e. throws an Exception when the Stack is Empty and operations such
    as pop, top etc are performed on the Stack.

---
+++ data-structures-and-algorithms-java/src/ds/StackEmptyException.java
@@ -0,0 +1,11 @@
+package ds;
+
+/**
+ *  Throws StackEmptyException
+ */
+public class StackEmptyException extends RuntimeException{
+    
+    public StackEmptyException(String err){
+        super(err);
+    }
+}
